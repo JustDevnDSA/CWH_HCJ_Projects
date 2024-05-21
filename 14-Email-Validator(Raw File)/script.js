e41076f865b67002f61ffdc2b3c5fb426542e040
DevnDSA <132866106+JustDevnDSA@users.noreply.github.com>
2024-05-21 19:59:10 +0530

    Add files via upload

---
+++ 14-Email-Validator(Raw File)/script.js
@@ -0,0 +1,82 @@
+// submitBtn.addEventListener("click", async () => {
+//   // e.preventDefault()
+//   console.log("cleicked");
+ 
+//   let email = document.getElementById("email").value;
+//   let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}ema_live_JFs5SvHjGcSMBPSwtKbRae5SZeMC6ACrsvRxZdX5`;
+  
+//   let res = await fetch(url)
+//   let result = await res.json()
+
+//   let str = ``;
+//   for (key of Object.keys(result)) {
+//     // console.log(key);
+//     str = str + `<div>${key} : ${result[key]}</div>`;
+//   }
+//   resultCont.innerHTML = str;
+// });
+
+// let result = {
+//   tag: "",
+//   free: false,
+//   role: false,
+//   user: "testing",
+//   email: "testing@codewithharry.com",
+//   score: 0.64,
+//   state: "undeliverable",
+//   domain: "codewithharry.com",
+//   reason: "invalid_mailbox",
+//   mx_found: true,
+//   catch_all: null,
+//   disposable: false,
+//   smtp_check: false,
+//   did_you_mean: "",
+//   format_valid: true,
+// };
+
+// // console.log(str);
+
+console.log("This is my script")
+
+let result = {
+    "tag": "",
+    "free": false,
+    "role": false,
+    "user": "akshaykumar",
+    "email": "akshaykumar@codewithharry.com",
+    "score": 0.64,
+    "state": "undeliverable",
+    "domain": "codewithharry.com",
+    "reason": "invalid_mailbox",
+    "mx_found": true,
+    "catch_all": null,
+    "disposable": false,
+    "smtp_check": false,
+    "did_you_mean": "",
+    "format_valid": true
+}
+
+
+
+submitBtn.addEventListener("click", async (e) => {
+    e.preventDefault()
+    console.log("Clicked!")
+    resultCont.innerHTML = `<img width="123" src="img/loading.svg" alt="">`
+    let key = "YOUR_API_KEY_HERE";
+    let email = document.getElementById("email").value 
+    let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
+    let res = await fetch(url)
+    let result = await res.json()
+    let str = ``
+    for (key of Object.keys(result)) {
+        if(result[key] !== "" && result[key]!== " "){ 
+            str = str + `<div>${key}: ${result[key]}</div>`
+        }
+    }
+
+    console.log(str)
+    resultCont.innerHTML = str
+})
+
+
+
